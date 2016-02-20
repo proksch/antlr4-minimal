4f8c06ce5cf4ad8e60c58a6b51f11a35117f925e
Sebastian Proksch <seb@Sebtop.fritz.box>
2016-02-20 15:08:26 +0100

    working validator for 'Hello World' grammar

---
+++ src/main/java/antlr4/minimal/generated/HelloWorldParser.java
@@ -0,0 +1,151 @@
+// Generated from HelloWorld.g by ANTLR 4.5.2
+
+/**
+ * Copyright 2016 Sebastian Proksch
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package antlr4.minimal.generated;
+
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class HelloWorldParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.5.2", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		SALUTATION=1, ENDSYMBOL=2;
+	public static final int
+		RULE_expression = 0;
+	public static final String[] ruleNames = {
+		"expression"
+	};
+
+	private static final String[] _LITERAL_NAMES = {
+		null, "'Hello world'", "'!'"
+	};
+	private static final String[] _SYMBOLIC_NAMES = {
+		null, "SALUTATION", "ENDSYMBOL"
+	};
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "HelloWorld.g"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public HelloWorldParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+	public static class ExpressionContext extends ParserRuleContext {
+		public TerminalNode SALUTATION() { return getToken(HelloWorldParser.SALUTATION, 0); }
+		public TerminalNode ENDSYMBOL() { return getToken(HelloWorldParser.ENDSYMBOL, 0); }
+		public ExpressionContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_expression; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof HelloWorldListener ) ((HelloWorldListener)listener).enterExpression(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof HelloWorldListener ) ((HelloWorldListener)listener).exitExpression(this);
+		}
+	}
+
+	public final ExpressionContext expression() throws RecognitionException {
+		ExpressionContext _localctx = new ExpressionContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_expression);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(2);
+			match(SALUTATION);
+			setState(3);
+			match(ENDSYMBOL);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\4\b\4\2\t\2\3\2\3"+
+		"\2\3\2\3\2\2\2\3\2\2\2\6\2\4\3\2\2\2\4\5\7\3\2\2\5\6\7\4\2\2\6\3\3\2\2"+
+		"\2\2";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
---
+++ src/test/java/antlr4/minimal/ValidationTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2016 Sebastian Proksch
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package antlr4.minimal;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ValidationTest {
+
+    private Validation sut;
+
+    @Before
+    public void setup() {
+        sut = new Validation();
+    }
+
+    @Test
+    public void ex1() {
+        assertValid("Hello world!");
+    }
+
+    @Test
+    public void ex2() {
+        assertValid("Hello World");
+    }
+
+    @Test
+    public void ex3() {
+        assertValid("Hello Word!");
+    }
+
+    private void assertValid(String input) {
+        assertTrue(sut.parse(input));
+    }
+
+    private void assertInvalid(String input) {
+        assertFalse(sut.parse(input));
+    }
+}
